bcc0b4b1e0322a4097b29a85e36aec328d557f52
Олег Матшин <89175955650@mail.ru>
2022-06-22 21:15:26 +0300

    Rename old files, and create new folder "tests".

--- Tests/test_hometask_220622.py
+++ Tests/test_hometask_220622.py
@@ -1,4 +1,8 @@
 import unittest
+from parameterized import parameterized
+from modified_app import check_document_existance, get_doc_owner_name, get_all_doc_owners_names, remove_doc_from_shelf
+from modified_app import add_new_shelf, append_doc_to_shelf, delete_doc, get_doc_shelf, move_doc_to_shelf
+from modified_app import show_document_info, show_all_docs_info, add_new_doc, secretary_program_start
 
 
 documents = [
